0cb970a2e901cb165342e6aaeea9e9c35d88dc3c
Artyom Gabtraupov <artyomgv05@gmail.com>
2024-06-23 00:22:24 -0400

    adde photography page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.scss';
 import { Tabs } from './components/Tabs';
 import { WillchaiButton } from './components/WillchaiButton';
+import { SocialsButtons } from './components/SocialsButtons';
 
 function App() {
     return (
         <div className="App">
             <div className="overlay"></div>
             <WillchaiButton/>
+            <SocialsButtons/>
             <Tabs/>
         </div>
     );
