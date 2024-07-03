4f7a72b7f06035f988e7c68f2e7a4ebe92297099
willreal <willreal888@gmail.com>
2024-07-03 18:54:20 -0400

    mobile view

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,20 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Contact.scss";
 
+const Notification = ({ message }) => {
+    return (
+        <div className="notification">
+            {message}
+        </div>
+    );
+};
+
 export const Contact = () => {
+    const [showNotification, setShowNotification] = useState(false);
 
     const copyToClipboard = () => {
         const textToCopy = "me@willchai.com";
         navigator.clipboard.writeText(textToCopy);
-        alert("Email copied to clipboard!");
+        setShowNotification(true);
+        setTimeout(() => {
+            setShowNotification(false);
+        }, 1000); // Hide notification after 3 seconds
     };
 
-    return (  
+    return (
         <div className="container-contact">
             <div className="business-card" onClick={copyToClipboard}>
                 <h1>me<i>@</i>willchai.com</h1>
                 <h2>don't be a stranger</h2>
             </div>
+            {showNotification && <Notification message="Email copied to clipboard!" />}
         </div>
     );
 }
