b36b2e23d1958420180cc1b08cee9bb5066c5f40
willreal <willreal888@gmail.com>
2024-07-31 00:09:09 -0400

    amazing description of the commit

--- src/pages/Photography.jsx
+++ src/pages/Photography.jsx
@@ -17,24 +17,17 @@ export const Photography = () => {
                     <a href="https://www.instagram.com/willchei" target="_blank" rel="noopener noreferrer"><p>Intersted in more?</p></a>
                 </div>
                 <div className="container-photography__sections__section2">
-                    <h1>Experience (2021 - Present)</h1>
+                    <h1>Experiences</h1>
                     <p>
-                        <b>VP of Cinematography {'\n'}</b>
-                        &#x2022; McMaster Film Prod. Club {'\n'}
-                        <b>Head of Videography {'\n'}</b>
-                        &#x2022; Health Science Musical {'\n'}
-                        <b>Camera Operator {'\n'}</b>
-                        &#x2022; HSM ‘24 {'\n'}
-                        &#x2022; McMaster Musical ‘24 - “Into the Woods” {'\n'}
-                        <b>Photographer {'\n'}</b>
-                        &#x2022; 4th Year Health Science Semi-Formal ‘24{'\n'} 
-                        &#x2022; SJK High School{'\n'} 
-                        &#x2022; Businesses: Foxwood Homes, Ace Shawarma & Burgers, various cafes{'\n'} 
-                        <b>Real Estate Photographer{'\n'} </b>
-                        &#x2022; Private homes{'\n'} 
-                        <b>Director of Photography / Cinematographer{'\n'}</b>
-                        &#x2022; Medium length film “Murder of Minus”{'\n'} 
-                    </p>
+                        <b>McMaster University {'\n'}</b>
+                        I hold the positions:  VP of Cinematography and Head of Videography in McMaster's biggest clubs, and have shot both photo and video for various concerts, films, musical productions, and beyond.{'\n'}{'\n'}
+                        <b>Real Estate{'\n'}</b>
+                        Shot images and walk-throughs primarily for private homes in Ontario, and was associated with Foxwood Homes (London). {'\n'}{'\n'}
+                        <b>Medium length film: “Murder of Minus”{'\n'}</b>
+                        I was the Director of Photography and Cinematographer for the entire production of this original film.{'\n'}{'\n'}
+                        <b>Familiar with:{'\n'}</b>
+                        Stills, video/film, promotional, and spatial 3D immersive.{'\n'}
+                        </p>
                 </div>
             </div>
         </div>
